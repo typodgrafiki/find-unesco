151a1873067f13e31cfc769de051cfc4f2ed5e63
typodgrafiki <53211722+typodgrafiki@users.noreply.github.com>
2023-09-21 13:16:48 +0200

    up

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,17 +1,41 @@
+'use client'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import "@/styles/newsletter.scss"
 
 const Newsletter: React.FC = () => {
+    
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        
+        try {            
+            const response = await axios.post('/api/newsletter', { email });
+            if (response.data.success) {
+                toast.success('Successfully subscribed to the newsletter!');
+            }else{
+                console.log('no1')
+            }
+        } catch (error) {
+          toast.error('An error occurred. Please try again.');
+        }
+    };
+  
     return (
         <>
             <section className="newsletter relative">
                 <div className="container">
                     <h4>Newsletter</h4>
                     <p>Sign up for the newsletter and receive the latest information</p>
-                    <form className="flex">
+                    <form onSubmit={handleSubmit}>
                         <input
-                            type="text"
                             className="formControl"
                             placeholder="john@example.com"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
                         <button
                             type="submit"
